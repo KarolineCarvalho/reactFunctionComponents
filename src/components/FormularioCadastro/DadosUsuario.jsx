2a9385d4be4f16a090f960ba40f1df203bb835f1
Karoline Carvalho <ka.olicarvalho@gmail.com>
2021-09-30 17:17:41 -0300

    Renderização condicional

--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,13 +1,19 @@
 import { Button, TextField } from "@material-ui/core";
 import React from "react";
 
-function DadosUsuario() {
+function DadosUsuario({ aoEnviar }) {
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        aoEnviar();
+      }}
+    >
       <TextField
         id="email"
         label="Email"
         type="email"
+        required
         variant="outlined"
         margin="normal"
         fullWidth
@@ -16,6 +22,7 @@ function DadosUsuario() {
         id="senha"
         label="Senha"
         type="password"
+        required
         variant="outlined"
         margin="normal"
         fullWidth
